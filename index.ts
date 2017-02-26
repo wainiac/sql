b0888cefd5c3216a601099325800bbb943f49e90
Wayne Muller <wayne@bid.ventures>
2017-02-25 23:27:14 -0500

    Test MySQL

--- index.ts
+++ index.ts
@@ -1,4 +1,12 @@
 import * as Promise from "bluebird";
+import * as MySQL from "mysql2/promise";
+
+
+let pool = MySQL.createPool({
+    user: "root"
+});
+
+console.log(pool);
 
 let resolve = (input: string): Promise<string> => {
     return Promise.resolve()
